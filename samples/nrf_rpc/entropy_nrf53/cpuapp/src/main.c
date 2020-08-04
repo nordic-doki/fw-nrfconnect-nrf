6fbb0221819835df181ee6288f41172567be83f6
Dominik Kilian <Dominik.Kilian@nordicsemi.no>
2020-08-04 08:36:26 +0200

    Speed test for custom RTT output

--- samples/nrf_rpc/entropy_nrf53/cpuapp/src/main.c
+++ samples/nrf_rpc/entropy_nrf53/cpuapp/src/main.c
@@ -30,19 +30,51 @@ static void result_callback(int result, u8_t *buffer, size_t length)
 	printk("\n");
 }
 
+#include <SEGGER_RTT.h>
+
+uint8_t rtt_buffer[8192];
+static SEGGER_RTT_BUFFER_UP *up;
+
+static void initialize(void)
+{
+	int i;
+
+	up = &_SEGGER_RTT.aUp[1];
+	up->sName = "test_rtt";
+	up->pBuffer = (char *)(&rtt_buffer[0]);
+	up->SizeOfBuffer = sizeof(rtt_buffer);
+	up->RdOff = 0u;
+	up->WrOff = 0u;
+	up->Flags = SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL;
+	for (i = 0; i < sizeof(rtt_buffer); i++)
+	{
+		rtt_buffer[i] = i;
+	}
+}
+
+
 void main(void)
 {
 	int err;
+	uint32_t p;
 
 	printk("Entropy sample started[APP Core].\n");
 
-	err = entropy_remote_init();
+	/*err = entropy_remote_init();
 	if (err) {
 		printk("Remote entropy driver initialization failed\n");
 		return;
-	}
+	}*/
 
 	printk("Remote init send\n");
+
+	initialize();
+	while (1) {
+		p = up->RdOff;
+		up->WrOff = (p > 0) ? p - 1 : sizeof(rtt_buffer) - 1;
+		//k_sleep(K_MSEC(1));
+		__asm__ volatile ("nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\n");
+	}
 
 	while (true) {
 		k_sleep(K_MSEC(2000));
--- samples/nrf_rpc/entropy_nrf53/cpunet/src/main.c
+++ samples/nrf_rpc/entropy_nrf53/cpunet/src/main.c
@@ -6,6 +6,28 @@
 
 #include <zephyr.h>
 
+#if 1
+#include <SEGGER_RTT.h>
+uint8_t rtt_buffer[8192];
+static SEGGER_RTT_BUFFER_UP *up;
+static void initialize(void)
+{
+	int i;
+
+	up = &_SEGGER_RTT.aUp[1];
+	up->sName = "test_rtt";
+	up->pBuffer = (char *)(&rtt_buffer[0]);
+	up->SizeOfBuffer = sizeof(rtt_buffer);
+	//up->RdOff = 0u;
+	//up->WrOff = 0u;
+	up->Flags = SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL;
+	for (i = 0; i < sizeof(rtt_buffer); i++)
+	{
+		rtt_buffer[i] = i;
+	}
+}
+#endif
+
 void main(void)
 {
 	/* The only activity of this application is interaction with the APP
@@ -14,4 +36,15 @@ void main(void)
 	 * and start at system boot.
 	 */
 	printk("Entropy sample started[NET Core].\n");
+	uint32_t p;
+
+	printk("Remote init send\n");
+
+	initialize();
+	while (1) {
+		p = up->RdOff;
+		up->WrOff = (p > 0) ? p - 1 : sizeof(rtt_buffer) - 1;
+		__asm__ volatile ("nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\n");
+	}
+
 }
